903e38f4af31ea7aa73ee7e6071c0336e7718276
ivyhu <ajianouc@gmail.com>
2022-04-29 13:26:16 -0700

    changing config for DB Pool

--- db/index.js
+++ db/index.js
@@ -8,6 +8,9 @@ const pool = new Pool({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 module.exports = {
   query: (text, params) => pool.query(text, params),
